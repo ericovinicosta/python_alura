be771c3c30505e5379d1e061e55c956326fb3efb
ericovinicosta <elcosta@gmail.com>
2023-02-02 20:43:20 -0300

    jogo da forca

--- forca.py
+++ forca.py
@@ -4,6 +4,19 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'banana'
+    acertou = False
+    enforcou = False
+
+    while(not acertou and not enforcou):
+        chute = str(input('Qual a letra desejada? ')).strip()
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print(chute, end=' ')
+            print('_', end='')
+        print('')
+        print('Jogando')
+
     print("Fim do jogo")
 
 if(__name__=="__main__"):
