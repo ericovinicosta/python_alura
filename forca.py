c55780a4524cf785060f242776fec5bc1d79a0a8
suporteerico <suporte@ericocosta.com.br>
2023-02-03 00:40:54 -0300

    mudança na forca

--- forca.py
+++ forca.py
@@ -7,14 +7,19 @@ def jogar():
     palavra_secreta = 'banana'
     acertou = False
     enforcou = False
+    letras_certas = []
+
+    for letra in palavra_secreta:
+        letras_certas.append('_')
 
     while(not acertou and not enforcou):
+        index = 0
         chute = str(input('Qual a letra desejada? ')).strip()
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print(chute, end=' ')
-            print('_', end='')
-        print('')
+                letras_certas[index]=chute
+            index += 1
+        print(letras_certas)
         print('Jogando')
 
     print("Fim do jogo")
